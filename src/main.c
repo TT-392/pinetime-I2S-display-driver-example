f2d9016b771423956ca395a194caaa42f2dabbd9
TT-392 <tt-392@protonmail.com>
2021-09-21 23:27:06 +0200

    main.c cleanup

--- src/main.c
+++ src/main.c
@@ -1,34 +1,10 @@
-#include "nrf.h"
 #include "nrf_gpio.h"
-#include "nrf_delay.h"
-
-#include "wdt.h"
 #include "display.h"
-#include "display_defines.h"
-#include "display_print.h"
 #include "battery.h"
-#include "frame.c"
-//#include "breakout.h"
-//#include "modules/date.c"
-//#include "modules/heart.c"
-#include "semihost.h"
-#include "statusbar.h"
-//#include "scrollMenu.h"
-#include "systick.h"
-#include "watchface.h"
-#include "date.h"
-#include "touch.h"
-#include "uart.h"
 #include "main_menu.h"
-#include <math.h>
-#include "date_adjust.h"
 #include "clock_pine.h"
-#include "breakout.h"
 #include "system.h"
 #include "power_manager.h"
-#include "steamLocomotive.h"
-
-static bool toggle = 1;
 
 int main(void) {
     clock_setup();
