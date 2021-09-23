dc2704d0d5399c28293a7687d9e0c71f32d65ddc
TT-392 <tt-392@protonmail.com>
2021-09-23 19:50:02 +0200

    replace battery filter with a schmitt triggered battery setup

--- src/utils/battery.c
+++ src/utils/battery.c
@@ -1,39 +1,9 @@
 #include "display_print.h"
 #include "battery_pine.h"
-
-float filteredPercentage = 0;
+#include <stdio.h>
 
 void battery_init () {
     battery_setup();
-
-    int flags;
-    float voltage;
-
-    battery_read(&flags, &voltage, &filteredPercentage);
-    battery_read(&flags, &voltage, &filteredPercentage);
-}
-
-void battery_draw_percent (int x, int y, uint16_t color_text, uint16_t color_bg) {
-    int flags;
-    float voltage, percent;
-
-    battery_read(&flags, &voltage, &percent);
-
-    filteredPercentage += 0.001 * (percent - filteredPercentage);
-
-    char batteryString[] = "000%";
-
-    batteryString[2] = ((int)filteredPercentage % 10) + 48;
-    batteryString[1] = (((int)filteredPercentage/10) % 10) + 48;
-    batteryString[0] = (((int)filteredPercentage/100) % 10) + 48;
-    if (filteredPercentage < 100) {
-        batteryString[0] = 32;
-        if (filteredPercentage < 10) {
-            batteryString[1] = 32;
-        }
-    }
-
-    drawString (x, y, batteryString, color_text, color_bg);
 }
 
 int battery_percent() {
@@ -41,9 +11,38 @@ int battery_percent() {
     float voltage, percent;
 
     battery_read(&flags, &voltage, &percent);
-    return percent;
 
-    //filteredPercentage += 0.001 * (percent - filteredPercentage);
+#define averaging_buffer_length 15
+    static int averaging_buffer_index = 0;
+    static float averaging_buffer[averaging_buffer_length];
 
-    //return (int)filteredPercentage;
+    averaging_buffer[averaging_buffer_index++] = percent;
+    averaging_buffer_index %= averaging_buffer_length;
+    double average_percentage = 0;
+
+    for (int i = 0; i < averaging_buffer_length; i++) {
+        average_percentage += averaging_buffer[i];
+    }
+    average_percentage /= averaging_buffer_length;
+
+    static int percentage_schmitt = 0;
+    static int prev_percentage_schmitt = 0;
+
+    while ((int)average_percentage > percentage_schmitt && (int)average_percentage > prev_percentage_schmitt) {
+        prev_percentage_schmitt = percentage_schmitt;
+        percentage_schmitt++;
+    }
+
+    while ((int)average_percentage < percentage_schmitt && (int)average_percentage < prev_percentage_schmitt) {
+        prev_percentage_schmitt = percentage_schmitt;
+        percentage_schmitt--;
+    }
+
+    return percentage_schmitt;
+}
+
+void battery_draw_percent (int x, int y, uint16_t color_text, uint16_t color_bg) {
+    char batteryString[5];
+    sprintf(batteryString, "%3d%%", battery_percent());
+    drawString (x, y, batteryString, color_text, color_bg);
 }
