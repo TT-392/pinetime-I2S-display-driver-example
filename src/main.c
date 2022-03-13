7e18bee1f18e03c969ed851cf721b9f7e173f0f4
TT-392 <TT-392@protonmail.com>
2022-03-13 22:31:36 +0100

    Make it so I2S transfers can be repeated and document code

--- src/main.c
+++ src/main.c
@@ -12,15 +12,6 @@
 #define COLOR_16bit 0x05
 #define COLOR_12bit 0x03
 
-void swapBytes(uint8_t *buffer, int size) {
-    assert(size % 2 == 0);
-    for (int i = 0; i < size / 2; i++) {
-        uint8_t temp = buffer[i*2];
-        buffer[i*2] = buffer[i*2 + 1];
-        buffer[i*2 + 1] = temp;
-    }
-}
-
 
 // send one byte over spi (using SPIM to simplify the init commands)
 void SPIM_send(bool mode, uint8_t byte) {
@@ -53,8 +44,8 @@ void SPIM_init() {
     NRF_SPIM0->PSEL.MISO = LCD_MISO;
 
     NRF_SPIM0->CONFIG = (SPIM_CONFIG_ORDER_MsbFirst  << SPIM_CONFIG_ORDER_Pos)|
-                        (SPIM_CONFIG_CPOL_ActiveLow << SPIM_CONFIG_CPOL_Pos) |
-                        (SPIM_CONFIG_CPHA_Trailing   << SPIM_CONFIG_CPHA_Pos);
+        (SPIM_CONFIG_CPOL_ActiveLow << SPIM_CONFIG_CPOL_Pos) |
+        (SPIM_CONFIG_CPHA_Trailing   << SPIM_CONFIG_CPHA_Pos);
 
     NRF_SPIM0->FREQUENCY = SPIM_FREQUENCY_FREQUENCY_M8 << SPIM_FREQUENCY_FREQUENCY_Pos;
 }
@@ -65,7 +56,7 @@ void I2S_init() {
     // setup CMD pin
     NRF_GPIOTE->CONFIG[1] = GPIOTE_CONFIG_MODE_Task << GPIOTE_CONFIG_MODE_Pos |
         GPIOTE_CONFIG_POLARITY_Toggle << GPIOTE_CONFIG_POLARITY_Pos |
-        LCD_COMMAND << GPIOTE_CONFIG_PSEL_Pos | 
+        LCD_COMMAND << GPIOTE_CONFIG_PSEL_Pos |
         GPIOTE_CONFIG_OUTINIT_Low << GPIOTE_CONFIG_OUTINIT_Pos;
 
     NRF_GPIOTE->TASKS_CLR[1] = 1;
@@ -88,13 +79,9 @@ void I2S_init() {
     NRF_I2S->CONFIG.FORMAT = 0;
 
 
-    // Use stereo 
+    // Use stereo
     NRF_I2S->CONFIG.CHANNELS = 0 << I2S_CONFIG_CHANNELS_CHANNELS_Pos;
 
-    // Configure pins
-    NRF_I2S->PSEL.SCK = (LCD_SCK << I2S_PSEL_SCK_PIN_Pos); 
-    NRF_I2S->PSEL.LRCK = (PIN_LRCK << I2S_PSEL_LRCK_PIN_Pos); 
-    NRF_I2S->PSEL.SDOUT = (LCD_MOSI << I2S_PSEL_SDOUT_PIN_Pos);
 
     NRF_PPI->CH[1].EEP = (uint32_t) &NRF_I2S->EVENTS_TXPTRUPD;
     NRF_PPI->CH[1].TEP = (uint32_t) &NRF_I2S->TASKS_STOP;
@@ -116,15 +103,23 @@ void SPIM_enable(bool enabled) {
 }
 
 void I2S_enable(bool enabled) {
-    if (enabled)
+    if (enabled) {
         NRF_I2S->ENABLE = I2S_ENABLE_ENABLE_Enabled << I2S_ENABLE_ENABLE_Pos;
-    else {
-        NRF_I2S->PSEL.SCK = I2S_PSEL_SCK_CONNECT_Disconnected; 
+
+        NRF_I2S->PSEL.SCK = (LCD_SCK << I2S_PSEL_SCK_PIN_Pos);
+        NRF_I2S->PSEL.LRCK = (PIN_LRCK << I2S_PSEL_LRCK_PIN_Pos);
+        NRF_I2S->PSEL.SDOUT = (LCD_MOSI << I2S_PSEL_SDOUT_PIN_Pos);
+    } else {
+        NRF_I2S->PSEL.SCK = I2S_PSEL_SCK_CONNECT_Disconnected;
+        NRF_I2S->PSEL.LRCK = I2S_PSEL_SCK_CONNECT_Disconnected;
+        NRF_I2S->PSEL.SDOUT = I2S_PSEL_SCK_CONNECT_Disconnected;
+
         NRF_I2S->ENABLE = I2S_ENABLE_ENABLE_Disabled << I2S_ENABLE_ENABLE_Pos;
         nrf_gpio_pin_write(LCD_SCK,1);
     }
 }
 
+// quick and dirty known working drawSquare
 void drawSquare(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint16_t color) {
     SPIM_enable(1);
 
@@ -154,36 +149,86 @@ void drawSquare(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint16_t col
     while (pixel != area) {
         SPIM_send(1, color >> 8);
         SPIM_send(1, color & 0xff);
-        
-        pixel++;
 
+        pixel++;
     }
     SPIM_enable(0);
 }
 
+static inline void swapBytes(uint8_t *buffer, int size) {
+    assert(size % 2 == 0);
+    for (int i = 0; i < size / 2; i++) {
+        uint8_t temp = buffer[i*2];
+        buffer[i*2] = buffer[i*2 + 1];
+        buffer[i*2 + 1] = temp;
+    }
+}
+
 void I2S_RAMWR(uint8_t* data, int pixCount)  {
+    // This I2S driver works by abusing the TXPTRUPD event as a PPI event to
+    // either stop the transfer or toggle the CMD pin. This event happens
+    // roughly 8.5 bytes before the end of a dma transfer, though, because half
+    // a byte is seen as no byte, and the CMD pin is sampled at the end of a
+    // byte, in practice, the action taken on EVENTS_TXPTRUPD has effect 9
+    // bytes (EVENTLEAD) before the end of the dma transfer. As an example of how
+    // this is done, take the following I2S transfer of 7 pixels:
+    //
+    // start[12] = {0x00, 0x00, RAMWR, pix1.1, pix1.2, pix2.1, pix2.2, pix3.1, pix3.2, pix4.1, pix4.2, pix5.1}
+    //                                   ^ *1
+    // buffer[16] = {pix5.2, pix6.1, pix6.2, pix7.1, pix7.2, pix1.1, pix1.2, dummy, dummy, dummy, dummy, dummy, dummy, dummy, dummy, dummy}
+    //                                                         ^ *3            ^ *2
+    //
+    // *1: The first TXPTRUPD event, the CMD pin will be set high here
+    // *2: The second TXPTRUPD event, the I2S transfer will be stopped here
+    // *3: The I2S dma transfer has to be a multiple of 4 bytes, therefore an
+    // uneven number ends with the first pixel repeated
+    //
+    // More notes:
+    //
+    // Because the I2S doesn't do 16MHz 8 bit transfers, it has to have a word
+    // size of 16 bytes at minimum, and, because of messed up endianness, the
+    // buffers in the example will need to have their endianness swapped by
+    // swapBytes() before the dma transfer.
+    //
+    // In reality, the transfer has 8 bytes and 1 bit of zeroes in front of it,
+    // therefore 7 bits are bitbanged at the start of the I2S transfer.
+    // Something similar goes for the end of the transfer, but this can be
+    // fixed by toggling the CS pin.
+
     assert(pixCount != 0);
 
-    SPIM_enable(0);
-    NRF_GPIOTE->TASKS_CLR[1] = 1;
+    const int EVENTLEAD = 9;
+
+    int buffSize = 16; // has to be divisable by 4
+
+    // The buffers contain extra space for trailing dummy bytes
+    uint8_t buffer1[buffSize + EVENTLEAD];
+    uint8_t buffer2[buffSize + EVENTLEAD];
+    uint8_t buffer3[buffSize + EVENTLEAD];
+    uint8_t *buffer = buffer1;
+    uint8_t start[12] = {0x00, 0x00, CMD_RAMWR};
+
     I2S_enable(1);
 
-    NRF_I2S->PSEL.SCK = I2S_PSEL_SCK_CONNECT_Disconnected; 
+    NRF_I2S->EVENTS_TXPTRUPD = 0;
+    NRF_I2S->EVENTS_STOPPED = 0;
+
+
+    NRF_GPIOTE->TASKS_CLR[1] = 1; // The command pin is connected to this GPIOTE channel
+
+    NRF_I2S->PSEL.SCK = I2S_PSEL_SCK_CONNECT_Disconnected;
 
     // bitbang 7 0's to fix bit offset
     for (int i = 0; i < 7; i++) {
         nrf_gpio_pin_write(LCD_SCK,1);
-        __NOP(); // may not be necessary
         nrf_gpio_pin_write(LCD_SCK,0);
     }
 
-    NRF_I2S->PSEL.SCK = LCD_SCK << I2S_PSEL_SCK_PIN_Pos; 
-
-    const int EVENTLEAD = 9; // EVENTS_TXPTRUPD happens 9 bytes before the end of a dma transfer, we use this event to toggle the CMD pin, and to stop the transfer
-    int buffSize = 16; // hast to be divisable by 4
+    NRF_I2S->PSEL.SCK = LCD_SCK << I2S_PSEL_SCK_PIN_Pos;
 
 
-    // We can only send an even number of pixels, if we have an uneven number, the first pixel is repeated at the end of the transfer
+    // We can only send an even number of pixels, in the case of an uneven
+    // number, the first pixel is repeated at the end of the transfer
     uint8_t lastPixel[2];
 
     if (pixCount % 2) {
@@ -194,22 +239,14 @@ void I2S_RAMWR(uint8_t* data, int pixCount)  {
         lastPixel[1] = data[pixCount*2 - 1];
     }
 
-    pixCount += pixCount % 2;
     int byteCount = (pixCount + (pixCount%2)) * 2;
 
-
-    NRF_I2S->EVENTS_TXPTRUPD = 0;
-    NRF_I2S->EVENTS_STOPPED = 0;
-
-
-    uint8_t buffer1[buffSize + EVENTLEAD];
-    uint8_t buffer2[buffSize + EVENTLEAD];
-    uint8_t buffer3[buffSize + EVENTLEAD];
-    uint8_t *buffer = buffer1;
-    uint8_t start[12] = {0x00, 0x00, 0x2C};
-
     int copyCount;
-    if (byteCount <= EVENTLEAD) { // there are EVENTLEAD bytes of pixel data in the first 12 byte buffer
+
+    // The max number of pixels in the start buffer is equal to EVENTLEAD
+    // because the cmd pin is toggled that many bytes before the end of the
+    // transfer
+    if (byteCount <= EVENTLEAD) {
         copyCount = byteCount-2;
         memcpy(start + 3 + copyCount, lastPixel, 2);
     } else {
@@ -218,14 +255,13 @@ void I2S_RAMWR(uint8_t* data, int pixCount)  {
 
     memcpy(start + 3, data, copyCount);
 
-
     byteCount -= EVENTLEAD;
     data += copyCount;
-
 
     bool first = true;
     while (byteCount + EVENTLEAD > 0) {
         int transferSize;
+
         assert(!((byteCount + EVENTLEAD) % 4));
 
         if (byteCount > 0) {
@@ -273,15 +309,25 @@ void I2S_RAMWR(uint8_t* data, int pixCount)  {
         else if (buffer == buffer2) buffer = buffer3;
         else buffer = buffer1;
     }
+
     while (!NRF_I2S->EVENTS_TXPTRUPD);
     NRF_I2S->EVENTS_TXPTRUPD = 0;
 
     NRF_PPI->CHENSET = 1 << PPI_I2S_TASKS_STOP;
     while (!NRF_I2S->EVENTS_STOPPED);
+
+    NRF_PPI->CHENCLR = 1 << PPI_GPIOTE_SET;
+    NRF_PPI->CHENCLR = 1 << PPI_I2S_TASKS_STOP;
+
+    I2S_enable(0);
+
+    // Transfer ends on a random bit, therefore CS has to be toggled to reset the bit counter
+    nrf_gpio_pin_write(LCD_SELECT,1);
+    nrf_gpio_pin_write(LCD_SELECT,0);
 }
 
-
 void drawBitmap_I2S(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint8_t *data) {
+    SPIM_enable(1);
 
     SPIM_send(0, CMD_CASET);
 
@@ -299,40 +345,18 @@ void drawBitmap_I2S(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint8_t 
     SPIM_send(1, y2 >> 8);
     SPIM_send(1, y2 & 0xff);
 
+    SPIM_enable(0);
+
     I2S_RAMWR(data, (x2-x1+1) * (y2-y1+1));
-
-
-    // Since we are not updating the TXD pointer, the sine wave will play over and over again.
-    // The TXD pointer can be updated after the EVENTS_TXPTRUPD arrives.
-    while (1) {
-        __WFE();
-    }
 }
 
 uint16_t convertColor(uint8_t r, uint8_t g, uint8_t b) {
-  return ((r >> 3) << 11 | (g >> 2) << 5 | (b >> 3));
+    return ((r >> 3) << 11 | (g >> 2) << 5 | (b >> 3));
 }
 
 int main() {
-    SPIM_enable(1);
-
     I2S_init();
     SPIM_init();
-
-    //RAMWR_I2S("no", 2);
-
-    /*
-    uint8_t data[100];
-    for (int i = 0; i < sizeof(data) / 2; i++) {
-        data[i*2] = (i+1)*0x10 + 0;
-        data[i*2+1] = (i+1)*0x10 + 1;
-    }*/
-    
-
-//    I2S_RAMWR(data, 30);
-
-
-    SPIM_enable(1);
 
     nrf_gpio_cfg_output(LCD_MOSI);
     nrf_gpio_cfg_output(LCD_SCK);
@@ -347,7 +371,7 @@ int main() {
     nrf_gpio_pin_write(LCD_BACKLIGHT_HIGH,0);
     nrf_delay_ms(1);
 
-    
+
     ///////////////////
     // reset display //
     ///////////////////
@@ -357,39 +381,46 @@ int main() {
     nrf_delay_ms(200);
     nrf_gpio_pin_write(LCD_SELECT,0);
 
+    SPIM_enable(1);
+
     SPIM_send (0, CMD_SWRESET);
     SPIM_send (0, CMD_SLPOUT);
 
     SPIM_send (0, CMD_COLMOD);
     SPIM_send (1, COLOR_16bit);
 
-    SPIM_send (0, CMD_MADCTL); 
+    SPIM_send (0, CMD_MADCTL);
     SPIM_send (1, 0x00);
 
     SPIM_send (0, CMD_INVON); // for standard 16 bit colors
     SPIM_send (0, CMD_NORON);
     SPIM_send (0, CMD_DISPON);
 
+    SPIM_enable(0);
 
-    //drawSquare(0, 0, 239, 239, 0x0000);
-    
-    int width = 150, height = 150;
+
+    drawSquare(0, 0, 239, 239, 0x0000);
+
+    int width = 120, height = 120;
 
     uint8_t data[width*height*2];
 
 
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
-            int brightness = (x * width / 256) ^ (y * height / 256);
-            uint16_t color = convertColor(brightness, brightness, brightness);
+            int g = ((255*x)/width) ^ ((255*y)/height);
+            int b = ((255*(width-x))/width) ^ ((255*y)/height);
+
+            uint16_t color = convertColor(0, g, b);
             data[(x + y*width) * 2] = color >> 8;
             data[(x + y*width) * 2 + 1] = color & 0xff;
         }
     }
 
-    //I2S_RAMWR(data, sizeof(data)/2);
-
     drawBitmap_I2S(0, 0, width - 1, height - 1, data);
+    drawBitmap_I2S(120, 120, 120 + width - 1, 120 + height - 1, data);
+    drawBitmap_I2S(120, 0, 120 + width - 1, height - 1, data);
+    drawBitmap_I2S(0, 120, width - 1, 120 + height - 1, data);
 
     while(1);
 }
